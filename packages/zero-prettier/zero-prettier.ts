b3a50a57fb772de96f275b0aa2b8a2fc66c7e750
Jigz <jigzpalillo@gmail.com>
2022-11-21 23:02:02 +0800

    [zero-prettier] Add zero-prettier.ts

---
+++ packages/zero-prettier/zero-prettier.ts
@@ -0,0 +1,14 @@
+export const zeroPrettier = {
+  _references: [
+    "https://www.npmjs.com/package/@zero-company/zero-prettier",
+    "https://github.com/zero-company/zero-community",
+  ],
+  $schema: "http://json.schemastore.org/prettierrc",
+  trailingComma: "all",
+  endOfLine: "lf",
+  arrowParens: "avoid",
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  jsxSingleQuote: true,
+};
